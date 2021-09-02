ff6fd01e1223a3a8dd4974b4ddb5a0d09ec7a558
Kevin Damian Aceves Siordia <kevin.aceves@hotmail.com>
2021-09-02 20:17:55 +0200

    Shut forgot to commit on updates, well, whatever

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # AskSFDC
-GUI based app that allows you to control your Ask Salesforce codes without needing to go to the Firebase Console, allows some extra functionalities like adding mails a bunch at a time, downloading records of scores, and more to come.
+GUI based app that allows you to control your Ask Salesforce certifications without needing to go to the Firebase Console, allows some extra functionalities like adding mails a bunch at a time, downloading records of scores, and more to come.
--- main.py
+++ main.py
@@ -1,9 +1,10 @@
-from os import error
+from re import A
 import firebase_admin
 from pathlib import Path
 import random
 from firebase_admin import credentials, auth, firestore
 import PySimpleGUI as gui
+import datetime
 
 #Save your google credentials in the same folder
 cred = credentials.Certificate("credentials.json")
@@ -17,35 +18,59 @@ def main():
         if event == "Exit" or event == gui.WIN_CLOSED:
             break
         elif event == "k_add_users_btn":
-            add_user(values['k_users_field'], win)
+            if(values['k_val_deladd_field'] != ""):
+                add_user(values['k_val_deladd_field'], win)
+            else:
+                alert_box("Fill in the requiered spaces")
             #win["k_history_box"].Update([f"Added user {values['k_users_field']}"])
+        elif event == "k_delete_users_btn":
+            if(values['k_val_deladd_field'] != ""):
+                del_users(values['k_val_deladd_field'], win)
+            else:
+                alert_box("Fill in the requiered spaces")
+        elif event == "k_search_btn":
+            search_users(values["k_val_search_field"], win)
+        elif event == "k_code_btn":
+            check_certification(values["k_val_code_field"], values["k_code_cb"], win)
 
     win.close()
     #show_menu()
 
 def layout_creator():
     add_users = [
-        gui.Text("Add Users"),
-        gui.In(size=(50,1), enable_events=True, key="k_users_field"),
+        gui.Text("Users/UID/File"),
+        gui.In(size=(50,1), enable_events=True, key="k_val_deladd_field"),
         gui.FileBrowse(key="k_users_folder"),
-        gui.Button("Add", key="k_add_users_btn")
+        gui.Button("Add", key="k_add_users_btn"),
+        gui.Button("Delete", key="k_delete_users_btn")
         ]
+    search_show_users = [
+        gui.Text("Searches for given user, if left blank shows all users"),
+        gui.In(size=(35,1), enable_events=True, key="k_val_search_field"),
+        gui.Button("Search", key="k_search_btn")
+    ]
+    code_search = [
+        gui.Text("Verify certification for given code"),
+        gui.In(size=(35,1), enable_events=True, key="k_val_code_field"),
+        gui.Button("Verify", key="k_code_btn"),
+        gui.Checkbox("Save to computer", default=False, key="k_code_cb")
+    ]
     action_history = [
         gui.Listbox(
-            values=[], enable_events=True, size = (50,20), key = "k_history_box"
+            values=[], enable_events=True, size = (80,10), key = "k_history_box"
         )
     ]
+    
     layout = []
-    layout.append([add_users, action_history])
+    layout.append([add_users, search_show_users, code_search, action_history])
     return layout
     
 def alert_box(alert_text):
-    gui.popup(alert_text)
+    gui.popup(alert_text, title="Alert")
 
 def add_user(text, win):
     if(Path(text).exists() and text.endswith('.txt')):
         #Create users from text file
-        print("Create users from file")
         file = open(text, 'rt')
         for line in file:
             create_user(line.lstrip().rstrip(), win) #Remove any leading whitespace to not get an error
@@ -55,7 +80,6 @@ def add_user(text, win):
     elif(text.find('@') != -1):
         #Is mail
         create_user(text.lstrip().rstrip(), win)
-        print("Adding single user")
     else:
         #Not a valid mail
         alert_box("Invalid mail")
@@ -69,125 +93,113 @@ def create_user(mail, win):
             password = str(random.randint(111111,999999)),
             disabled = False
         )
-        msg_cur = [f"Added new email {mail} succesfully"]
-        print("Added successfully")
+        msg_cur = f"Added new email {mail} succesfully"
         auth.generate_password_reset_link(new_user)
     except auth._auth_utils.EmailAlreadyExistsError:
-        msg_cur = [f"Email already exists {mail}"]
-        print(f"Email already exists {mail}")
+        msg_cur = f"Email already exists {mail}"
     except:
-        msg_cur = [f"Error occured while adding mail {mail}"]
+        msg_cur = f"Added new email {mail} succesfully"
+    elements_list.append(msg_cur)
+    win["k_history_box"].update(elements_list)
+    
+
+def delete_users(uid, win):
+    elements_list = win["k_history_box"].get_list_values()
+    msg_cur = ""
+    try:
+        auth.delete_user(uid=uid)
+        msg_cur = f"Deleted user {uid}"
+    except firebase_admin._auth_utils.UserNotFoundError:
+        msg_cur =  f"User not found {uid}"
+    except:
+        msg_cur = f"Error occured while deleting {uid}"
         pass
     elements_list.append(msg_cur)
     win["k_history_box"].update(elements_list)
 
-        
-'''
-def show_menu():
-    menu = {}
-    menu['1'] = "Add users"
-    menu['2'] = "Delete users"
-    menu['3'] = "Show users"
-    menu['4'] = "Verify user certification"
-    menu['5'] = "Exit"
 
-    while True:
-        options = menu.keys()
-        print("\n")
-        for entry in options:
-            print(entry, menu[entry])
-
-        sel = input("Please Select: ")
-        print("\n")
-        if sel == '1':
-            add_user()
-        elif sel == '2':
-            delete_user()
-        elif sel == '3':
-            show_users()
-        elif sel == '4':
-            check_certification()
-        elif sel == '5':
-            break
-        else:
-            print("Invalid option")
-
-
-def add_user():
-    opc = input("Enter filePath with emails or email: ")
-    if(Path(opc).exists()):
-        file = open(opc, "r")
+def del_users(text, win):
+    if(Path(text).exists() and text.endswith('.txt')):
+        #Delete users from text file
+        file = open(text, 'rt')
         for line in file:
-            create_user(line.lstrip().rstrip())
+            delete_users(line.lstrip().rstrip(), win) #Remove any leading whitespace to not get an error
+    elif(Path(text).exists()):
+        #File is not .txt raise error
+        alert_box("Invalid file")
     else:
-        create_user(opc)
-        
-def create_user(mail):
-    try:
-        new_user = auth.create_user(
-        email = mail,
-        password = str(random.randint(1000000, 9999999)),
-        disabled = False)
-        print('Successfully created new user: {0}'.format(new_user.email))
-        auth.generate_password_reset_link(new_user.email)
-    except error:
-        print('Unable to create account: {0}'.format(mail))
+        #Might be uid
+        delete_users(text.lstrip().rstrip(), win)
 
-
-def delete_user():
-    opc = input("Enter filePath with UID or UID: ")
-    if(Path(opc).exists()):
-        file = open(opc, "r")
-        for line in file:
-            erase_user(line.lstrip().rstrip())
+def search_users(text, win):
+    if(text == ""):
+        try:
+            page = auth.list_users()
+            while page:
+                page = page.get_next_page()
+            elements_list = win["k_history_box"].get_list_values()
+            msg = ""
+            for user in auth.list_users().iterate_all():
+                ts = float(user.user_metadata.last_sign_in_timestamp)
+                ts = ts/1000
+                msg = f"Mail: {user.email}, UID: {user.uid}, Last Accesed: {datetime.datetime.utcfromtimestamp(ts).strftime('%Y-%m-%dT%H:%M:%SZ')}"
+                elements_list.append(msg)
+            
+            win["k_history_box"].update(elements_list)
+        except:
+            alert_box(f"Unable to get fetch users")
+    elif(text.find('@') != -1):
+        try:
+            user = auth.get_user_by_email(text)
+            elements_list = win["k_history_box"].get_list_values()
+            ts = float(user.user_metadata.last_sign_in_timestamp)
+            ts = ts/1000
+            msg = f"Mail: {user.email}, UID: {user.uid}, Last Accesed: {datetime.datetime.utcfromtimestamp(ts).strftime('%Y-%m-%dT%H:%M:%SZ')}"
+            elements_list.append(msg)
+            win["k_history_box"].update(elements_list)
+        except:
+            alert_box(f"Unable to get user with mail: {text}")
     else:
-        erase_user(opc)
+        try:
+            user = auth.get_user(text)
+            elements_list = win["k_history_box"].get_list_values()
+            ts = float(user.user_metadata.last_sign_in_timestamp)
+            ts = ts/1000
+            msg = f"Mail: {user.email}, UID: {user.uid}, Last Accesed: {datetime.datetime.utcfromtimestamp(ts).strftime('%Y-%m-%dT%H:%M:%SZ')}"
+            elements_list.append(msg)
+            win["k_history_box"].update(elements_list)
+        except:
+            alert_box(f"Unable to get user with UID: {text}")
 
-def erase_user(id):
-    try:
-        auth.delete_user(uid=id)
-        print('Successfully deleted user: {0}'.format(id))
-    except:
-        print('Unable to delete user: {0}'.format(id))
-
-
-def show_users():
-    page = auth.list_users()
-    while page:
-        page = page.get_next_page()
-
-    for user in auth.list_users().iterate_all():
-        if(user.user_metadata.last_sign_in_timestamp != None):
-            print("User: {0}".format(user.email))
-        else:
-            print("HAS NOT BEEN ACCESSED User: {0}".format(user.email))
-
-
-def check_certification():
-    code = input("Enter code to verify: ")
+def check_certification(code, save, win):
+    has_one = False
     code = code.upper()
     users_colection = db.collection(u'userScores')
     docs = users_colection.stream()
-    download_data = False
     file_path = "CertificationScores" + code + ".csv"
-    if(input("Want to download the data? Enter 1: ") == "1"):
-        download_data = True
+    if(save):
         if(Path(file_path).exists()):
             file = open(file_path, "w")
         else:
             file = open(file_path, "x")
         file.write("UID     ,Mail   ,Score,Win,Time to Answer\n")
-            
+    elements_list = []
     for doc in docs:
         dict_doc = doc.to_dict()
-        if(doc.id.endswith(code)):
+        if(doc.id.endswith(code) and code != ""):
+            has_one = True
             if(dict_doc.get("Win")):
-                print(f"{dict_doc['Mail']} has certified with a score of {dict_doc['Score']} in {dict_doc['timeFinish'] - dict_doc['timeBegin']}")
+                elements_list.append(f"{dict_doc['Mail']} has certified with a score of {dict_doc['Score']} in {dict_doc['timeFinish'] - dict_doc['timeBegin']}")
             else:
-                print(f"{dict_doc['Mail']} hasn't certified with a score of {dict_doc['Score']} in {dict_doc['timeFinish'] - dict_doc['timeBegin']}")
-            if(download_data):
+                elements_list.append(f"{dict_doc['Mail']} hasn't certified with a score of {dict_doc['Score']} in {dict_doc['timeFinish'] - dict_doc['timeBegin']}")
+
+            if(save):
                 file.write(f"{doc.id.lstrip(code.upper())},{dict_doc['Mail']},{dict_doc['Score']},{dict_doc['Win']}, {dict_doc['timeFinish'] - dict_doc['timeBegin']}\n")
-'''
+    if(has_one):
+        win["k_history_box"].update(elements_list)
+    else:
+        win["k_history_box"].update([f"No certifications on record for code {code}"])
+
 if __name__ == "__main__":
     main()
 
